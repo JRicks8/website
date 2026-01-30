58a63b0a88517a47e07efbecde832ced450c3048
Jared Ricks <jvaun20@gmail.com>
2026-01-29 21:27:45 -0500

    Added to vite config to rollup html files into the build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import { globSync } from "glob";
+
+const pages = Object.fromEntries(
+  globSync("src/**/index.html").map(file => {
+    const name = file
+      .replace(/^src\//, "")
+      .replace(/\/index\.html$/, "") || "index";
+
+    return [name, resolve(__dirname, file)];
+  })
+);
 
 export default defineConfig({
   root: "src",
   base: "./",
   build: {
     outDir: "../dist", // Relative to root defined above ^
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: pages
+    }
   }
 });
