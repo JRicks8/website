6e117ffee35424c0498b5b8b4f3a20b7f3184663
Jared Ricks <jvaun20@gmail.com>
2026-04-29 21:18:41 -0700

    Made an entity builder for building entities

--- src/js/pages/physics.js
+++ src/js/pages/physics.js
@@ -1,9 +1,9 @@
-import { PerspectiveCamera, Raycaster, Scene, Vector2 as ThreeV2, WebGLRenderer } from "three";
+import { BoxGeometry, Mesh, MeshNormalMaterial, PerspectiveCamera, Raycaster, Scene, SphereGeometry, Vector2 as ThreeV2, WebGLRenderer } from "three";
 import { resizeRenderView } from "../projects/common/util/three-utils.js";
 import { GameState } from "../projects/common/game/game-state.js";
 import { TickProcess } from "../projects/common/processes/tick-process.js";
 import { EventDispatcher } from "../projects/common/event/event-dispatcher.js";
-import { buildDraggableBody } from "../projects/birds/factory/draggable-body-factory.js";
+import { entityBuilder } from "../projects/birds/builder/entity-builder.js";
 import { Registry } from "../projects/common/game/register.js";
 import { MeshRenderingProcess } from "../projects/common/processes/mesh-rendering-process.js";
 import { PhysicsProcess } from "../projects/common/processes/physics-process.js";
@@ -17,7 +17,10 @@ import { ScriptProcess } from "../projects/common/processes/script-process.js";
 import { getMouseCoordsFromPixel } from "../projects/common/util/window-utils.js";
 import { LAYER_IGNORE_ALL, LAYER_IGNORE_RAYCAST } from "../projects/common/util/layers.js";
 import { DebugProcess } from "../projects/common/processes/debug-process.js";
-import { COMP_RIGIDBODY, Restraint, RESTRAINT_POSITION, RESTRAINT_POSITION_AXIS, RigidbodyComponent } from "../projects/common/components/rigidbody.js";
+import { RigidbodyComponent } from "../projects/common/components/rigidbody.js";
+import { BoxColliderComponent } from "../projects/common/components/collider/box-collider.js";
+import { DraggableComponent } from "../projects/common/components/draggable-body.js";
+import { MeshRendererComponent } from "../projects/common/components/mesh-renderer.js";
 
 // threejs init
 const camera = new PerspectiveCamera(70, 16/9, 0.01, 100);
@@ -70,22 +73,35 @@ DebugProcess.initialize(scene);
 
 Registry.setContext('physics');
 
-const b1 = buildDraggableBody(Registry.getUniqueId(), scene, 0.5);
-Registry.register(b1, b1.id);
-b1.transform.position.set(0, 0, 0);
-{
-  /** @type {RigidbodyComponent} */
-  const rb = ComponentManager.getComponent(b1, COMP_RIGIDBODY);
-  const r = new Restraint();
-  r.type = RESTRAINT_POSITION;
-  r.components = { x: true, y: true, z: true };
-  r.vector.set(0, 0, 0);
-  rb.restraints.push(r);
-}
+const entity1 = entityBuilder()
+  .colliderComponent(new BoxColliderComponent(), scene)
+  .draggableComponent(new DraggableComponent())
+  .meshRendererComponent(
+    new MeshRendererComponent(
+      new Mesh(
+        new BoxGeometry(),
+        new MeshNormalMaterial()
+      )
+    ), scene
+  )
+  .rigidbodyComponent(new RigidbodyComponent())
+  .build();
+entity1.transform.position.set(1, 0, -5)
 
-const b2 = buildDraggableBody(Registry.getUniqueId(), scene, 2);
-Registry.register(b2, b2.id);
-b2.transform.position.set(-2, 0, -5);
+const entity2 = entityBuilder()
+  .colliderComponent(new BoxColliderComponent(), scene)
+  .draggableComponent(new DraggableComponent())
+  .meshRendererComponent(
+    new MeshRendererComponent(
+      new Mesh(
+        new BoxGeometry(),
+        new MeshNormalMaterial()
+      )
+    ), scene
+  )
+  .rigidbodyComponent(new RigidbodyComponent())
+  .build();
+entity2.transform.position.set(-1, 0, -5);
 
 // Player setup -----------------------------------------------
 PlayerProcess.setup(scene, camera);
